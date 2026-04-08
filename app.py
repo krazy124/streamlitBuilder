603dd5de630b0729da1806d93c765b45832b1c10
krazy124 <william.meredith4@gmail.com>
2026-04-08 17:32:28 -0500

    update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Streamlit App Builder v3", layout="wide")
 
 
 # =========================================================
-# HELPERS
+# HELPERS This is a change  
 # =========================================================
 def make_safe_variable_name(text: str) -> str:
     text = text.strip().lower()
